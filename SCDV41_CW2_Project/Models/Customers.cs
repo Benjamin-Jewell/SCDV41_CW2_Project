1144603e0b70834e9bff6d9a4a9761207f6c0941
Benjamin-Jewell <b.d.jewell@icloud.com>
2026-04-24 12:19:33 +0100

    Add project files.

---
+++ SCDV41_CW2_Project/Models/Customers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCDV41_CW2_Project.Models
+{
+    internal class Customers
+    {
+    }
+}
